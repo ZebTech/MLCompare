41dc859a6bacdb4e950ade1bb833c9b9867f46a3
seba-1511 <seba-1511@hotmail.com>
2014-04-25 18:55:57 +0200

    Added some of functions' code.

--- MLCompare.py
+++ MLCompare.py
@@ -2,22 +2,26 @@
 
 import scipy as sp
 import numpy as np
+from sklearn import datasets, svm, neighbors, linear_model
 
 from multiprocessing import Process, Manager
+
+"""
+A helper class, to train and compare classifiers in parallel.
+"""
 
 
 class Learner():
 
-    def __init__(self, algorithm, trainset):
-        self.algorithm = algorithm
-        self.trainset =
-        self.process = Process(target=parallel_train, args=())
+    def __init__(self, algorithm, dataset, targets):
+        self.algorithm = algorithm[0]
+        self.name = algorithm[1]
+        self.dataset = dataset
+        self.targets = targets
+        self.process = Process(target=self.parallel_train, args=())
 
     def parallel_train(self, data):
         print 'Not sure if useful or not yet.'
-
-
-linear = Learner('asdf')
 
 # TODO:
 # - By only calling MLCompare, you view of algorithm is performing better.
@@ -27,7 +31,21 @@ linear = Learner('asdf')
 # - Try almost all parameters for each algorithm. (Check Grid-Search)
 # - Finish by showing a  nice overview for each algorithm.
 # - Optional: Could be nice to also have some graphs ?
+#
+# For each classifier, output this example line:
+# SVM   Correct: 0.87  Time: 5.46s  Parameters: C=0.5, M=4.67, Kernel='rbf'
 
 
 def MLCompare(dataset, targets):
-    print 'To be executed to compare the ML Algorithms on a given dataset'
+    algorithms = [
+        ('SVM', svm.SVC),
+        ('kNN', neighbors.KNeighborsClassifier),
+        ('Logistic Classifier', linear_model.LogisticRegression),
+    ]
+
+    learners = [Learner(x, dataset, targets) for x in algorithms]
+    print learners
+
+# For testing purposes:
+data = datasets.load_iris()
+MLCompare(data.data, data.target)
