944b04a4449bb2d19a34d5baf38ffd5a2d9969a8
seba-1511 <seba-1511@hotmail.com>
2014-05-01 17:35:21 +0200

    Scoring and optimal parameters search works.

--- MLCompare.py
+++ MLCompare.py
@@ -9,7 +9,7 @@ from sklearn.cross_validation import KFold, cross_val_score
 from multiprocessing import Process, Manager
 
 # Show the debug messages ?
-DEBUG = False #True
+DEBUG = False  # True
 
 # Computer Settings
 NB_THREADS = 4
@@ -33,33 +33,29 @@ class Learner():
          self.train_targets,
          self.test_data,
          self.test_targets) = self.buildSets(features, targets)
-        self.optimal_params = 1.0
+        self.optimal_algo = self.algorithm()
         self.train = Process(
-            target=self.parallelTrain, args=(self.train_data,))
+            target=self.parallelTrain, args=(self.train_data, self))
 
-    def parallelTrain(self, data):
+    def parallelTrain(self, data, Learner):
         # self.algorithm.fit(self.train_data, self.train_targets)
         # scores = cross_val_score(
         #     self.algorithm, self.train_data, self.train_targets, n_jobs=NB_THREADS)
         # scores = np.mean(scores)
-        self.optimal_params = self.findBestParams()
-        self.averaged_score = self.findAverageCVResults()
+        Learner.optimal_algo = Learner.findBestParams()
+        Learner.averaged_score = Learner.findAverageCVResults()
         if DEBUG:
             message = 'Fitted %s with training scores %s'
-            print message % (self.name, self.averaged_score)
+            print message % (Learner.name, Learner.averaged_score)
 
     def findBestParams(self):
         clf = GridSearchCV(
             self.algorithm(), self.parameters, n_jobs=NB_THREADS, cv=GRID_SEARCH_CV)
         clf.fit(self.train_data, self.train_targets)
-        print(clf.best_estimator_)
-        print()
-        print("Grid scores on development set:")
-        print()
-        for params, mean_score, scores in clf.grid_scores_:
-            print("%0.3f (+/-%0.03f) for %r"
-                  % (mean_score, scores.std() / 2, params))
-        print()
+        if DEBUG:
+            message = 'The optimal algorithm for %s is: %s'
+            print message % (self.name, self.optimal_algo)
+        return clf.best_estimator_
 
     def findAverageCVResults(self):
         print 'To implement'
@@ -85,13 +81,15 @@ class Learner():
             print 'Test set size: %s inputs.' % len(test_data)
         return (train_data, train_targets, test_data, test_targets)
 
-    def scoreLearner(self):
-        # self.algorithm(self.optimal_params).fit(
-        #     self.train_data, self.train_targets)
-        self.algorithm().fit(self.train_data, self.train_targets)
-        score = 0.34  # self.algorithm.score(self.test_data, self.test_targets)
+    def score(self):
+        self.optimal_algo.fit(self.train_data, self.train_targets)
+        score = self.optimal_algo.score(self.test_data, self.test_targets)
         message = '%s:      Correct: %s     Time: 0     Parameters: %s'
-        print message % (self.name, score, self.optimal_params)
+        print ''
+        print ''
+        print message % (self.name, score, self.optimal_algo)
+        print ''
+        print ''
 
 # TODO:
 # - By only calling MLCompare, you view of algorithm is performing better.
@@ -121,7 +119,11 @@ def MLCompare(features, targets):
 
     [l.train.join() for l in learners]
 
-    [l.scoreLearner() for l in learners]
+    print ''
+    print ''
+    print 'RESULTS:'
+
+    [l.score() for l in learners]
 
 # For testing purposes:
 data = datasets.load_iris()
