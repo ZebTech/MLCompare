e334bb374fa6b4a5775ea552ba9c60ef3b232ca4
seba-1511 <seba-1511@hotmail.com>
2014-05-01 17:52:14 +0200

    Finding averaged training works, but communicating from child thread to main doesn't.

--- MLCompare.py
+++ MLCompare.py
@@ -9,7 +9,7 @@ from sklearn.cross_validation import KFold, cross_val_score
 from multiprocessing import Process, Manager
 
 # Show the debug messages ?
-DEBUG = False  # True
+DEBUG = True
 
 # Computer Settings
 NB_THREADS = 4
@@ -17,6 +17,7 @@ NB_THREADS = 4
 # Application settings
 TEST_SET_PERCENTAGE = 0.1
 GRID_SEARCH_CV = 4
+KFOLD_NB_FOLDS = 15
 
 
 class Learner():
@@ -34,32 +35,34 @@ class Learner():
          self.test_data,
          self.test_targets) = self.buildSets(features, targets)
         self.optimal_algo = self.algorithm()
+        self.averaged_score = 0.0
         self.train = Process(
             target=self.parallelTrain, args=(self.train_data, self))
 
     def parallelTrain(self, data, Learner):
-        # self.algorithm.fit(self.train_data, self.train_targets)
-        # scores = cross_val_score(
-        #     self.algorithm, self.train_data, self.train_targets, n_jobs=NB_THREADS)
-        # scores = np.mean(scores)
         Learner.optimal_algo = Learner.findBestParams()
         Learner.averaged_score = Learner.findAverageCVResults()
-        if DEBUG:
-            message = 'Fitted %s with training scores %s'
-            print message % (Learner.name, Learner.averaged_score)
 
     def findBestParams(self):
         clf = GridSearchCV(
             self.algorithm(), self.parameters, n_jobs=NB_THREADS, cv=GRID_SEARCH_CV)
         clf.fit(self.train_data, self.train_targets)
         if DEBUG:
+            print ''
             message = 'The optimal algorithm for %s is: %s'
-            print message % (self.name, self.optimal_algo)
+            print message % (self.name, clf.best_estimator_)
         return clf.best_estimator_
 
     def findAverageCVResults(self):
-        print 'To implement'
-        return 0.20
+        cross_validator = KFold(len(self.train_data), n_folds=KFOLD_NB_FOLDS)
+        scores = cross_val_score(self.optimal_algo, self.train_data,
+                                 self.train_targets, n_jobs=NB_THREADS, cv=cross_validator)
+        scores = np.mean(scores)
+        if DEBUG:
+            print ''
+            message = 'Fitted %s with training scores %s'
+            print message % (self.name, scores)
+        return scores
 
     def buildSets(self, features, targets):
         # Shuffling the data
@@ -77,6 +80,7 @@ class Learner():
         test_data, test_targets = features[
             test_length:], targets[test_length:]
         if DEBUG:
+            print ''
             print 'Train set size: %s examples.' % len(train_data)
             print 'Test set size: %s inputs.' % len(test_data)
         return (train_data, train_targets, test_data, test_targets)
@@ -84,10 +88,10 @@ class Learner():
     def score(self):
         self.optimal_algo.fit(self.train_data, self.train_targets)
         score = self.optimal_algo.score(self.test_data, self.test_targets)
-        message = '%s:      Correct: %s     Time: 0     Parameters: %s'
+        message = '%s:      Correct: %s     Average: %s     Time: 0     Parameters: %s'
         print ''
         print ''
-        print message % (self.name, score, self.optimal_algo)
+        print message % (self.name, score, self.averaged_score, self.optimal_algo)
         print ''
         print ''
 
@@ -106,11 +110,11 @@ class Learner():
 
 def MLCompare(features, targets):
     algorithms = [
-        ('SVM', svm.SVC, [{'C': [1, 10, 100, 1000, 10000]}]),
+        ('SVM', svm.SVC, [{'C': [1.01, 10.01, 100.01, 1000.01, 10000.01]}]),
         # ('kNN', neighbors.KNeighborsClassifier,
         #  [{'C': [1, 10, 100, 1000, 10000]}]),
         ('Logistic Classifier', linear_model.LogisticRegression,
-         [{'C': [1, 10, 100, 1000, 10000]}]),
+         [{'C': [1.01, 10.01, 100.01, 1000.01, 10000.01]}]),
     ]
 
     learners = [Learner(x, features, targets) for x in algorithms]
